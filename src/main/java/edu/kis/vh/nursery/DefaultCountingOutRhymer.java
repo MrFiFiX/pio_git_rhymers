ea2e1597bf47b6ed0fa939d7918debc558397fcb
VladaParkhomenko <252503@edu.p.lodz.pl>
2025-03-17 15:50:34 +0100

    Refactor: Extract constants and add final modifier

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private static final int ARRAY_SIZE = 12;  
+    private static final int EMPTY_TOTAL = -1;
 
-    public int total = -1;
+    private final int[] numbers = new int[ARRAY_SIZE];  
+
+    public int total = EMPTY_TOTAL;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_TOTAL;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == ARRAY_SIZE - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return EMPTY_TOTAL;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return EMPTY_TOTAL;
         return numbers[total--];
     }
 
