b249e1b39da383243da7011f2744c7f91c473846
VladaParkhomenko <252503@edu.p.lodz.pl>
2025-03-27 11:31:50 +0100

    8. Dodano adnotację @Override tam, gdzie to konieczne.

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,17 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejectedCount = 0;
+	int totalRejectedCount = 0;
 
-    public int reportRejected() {
-        return totalRejectedCount;
-    }
+	public int reportRejected() {
+		return totalRejectedCount;
+	}
 
-    public void countIn(int in) {
-        if (!callCheck() && in > peekaboo())
-            totalRejectedCount++;
-        else
-            super.countIn(in);
-    }
+	@Override
+	public void countIn(int in) {
+		if (!callCheck() && in > peekaboo())
+			totalRejectedCount++;
+		else
+			super.countIn(in);
+	}
 }
