342282ec505e7ef7b6f795c1d9f861551661126d
Filip C <252788@edu.p.lodz.pl>
2025-04-06 08:28:45 +0200

    14. Klasa Node przeniesiona jako podklasa dla IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,37 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+    protected static class Node {
+
+        private final int valueNode;
+        private Node prevNode;
+        private Node nextNode;
+
+        public Node(int i) {
+            valueNode = i;
+        }
+
+        public int getValueNode() {
+            return valueNode;
+        }
+
+        public Node getPrevNode() {
+            return prevNode;
+        }
+
+        public void setPrevNode(Node prevNode) {
+            this.prevNode = prevNode;
+        }
+
+        public Node getNextNode() {
+            return nextNode;
+        }
+
+        public void setNextNode(Node nextNode) {
+            this.nextNode = nextNode;
+        }
+    }
+
     private Node lastNode;
     private int currentIndex;
 
